a7e7a1740c0d8e42e3615327eeacd9105ad7cf79
welcome <welcome@192.168.0.7>
2017-04-24 19:11:00 +0530

    first commit

---
+++ src/second/tushar.java
@@ -0,0 +1,13 @@
+package second;
+import java.util.Scanner;
+public class tushar 
+	{
+		public static void main(String[] args)
+		{
+		
+		Scanner tushar= new Scanner(System.in);
+		System.out.println(tushar.next());
+		
+	}
+
+}
